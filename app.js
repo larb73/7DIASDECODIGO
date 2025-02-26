f8bd64e92cd58aa3f9e9534fb792d184778d2d88
Laura Rodriguez <larb260402@gmail.com>
2025-02-26 20:25:55 -0300

    segundo dia de Js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const nombre = prompt("Por favor , ¿Cuál es tu nombre?");
+const edad = prompt(" ¿Cuántos años tienes?");
+const estu = prompt("¿Qué lenguaje de programación estás estudiando?")
+
+alert("¡Hola, Bienvenidos...En este segundo dia , necesitamos algunos datos de ustedes los usuairos!");
+
+
+const msg = "Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]! ¡Que gusto ${nombre}!, permiteme felicitarte que hayas elegido estudiar ${estu} , es fabuluso con tus ${edad} años";
+
+alert(msg);
+
+- 
+
+ 
+
