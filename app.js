eb4d0de5309a9849cbe33b6513e45f5d3528a49a
Larb73 <larb260402@gmail.com>
2025-02-26 22:01:41 -0300

    actual segundo dia

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
-const nombre = prompt("Por favor , ¿Cuál es tu nombre?");
-const edad = prompt(" ¿Cuántos años tienes?");
-const estu = prompt("¿Qué lenguaje de programación estás estudiando?")
-
 alert("¡Hola, Bienvenidos...En este segundo dia , necesitamos algunos datos de ustedes los usuairos!");
 
+const nombre = prompt("Por favor , ¿Cuál es tu nombre?");
+const edad = prompt(" ¿Cuántos años tienes?");
+const lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?")
 
-const msg = "Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]! ¡Que gusto ${nombre}!, permiteme felicitarte que hayas elegido estudiar ${estu} , es fabuluso con tus ${edad} años";
+const mensaje = (`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
+alert(mensaje);
 
-alert(msg);
 
-- 
+let respuesta = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
 
- 
+if (respuesta == "1") {
+    alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+} else if (respuesta == "2") {
+    alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
+} else {
+    alert("Respuesta no válida. Debes responder con 1 o 2.");
+}
+
 
