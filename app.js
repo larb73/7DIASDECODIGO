acbb124a2c5ca0be5eaffca831a9e4ce3032f6d5
Larb73 <larb260402@gmail.com>
2025-02-27 22:21:38 -0300

    ACTUALIZANDO

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 alert("¡Hola, Bienvenidos...En este segundo dia , necesitamos algunos datos de ustedes los usuairos!");
 
 const nombre = prompt("Por favor , ¿Cuál es tu nombre?");
@@ -6,10 +5,10 @@ const edad = prompt(" ¿Cuántos años tienes?");
 const lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?")
 
 const mensaje = (`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
+
 alert(mensaje);
 
-
-let respuesta = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
+const respuesta = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
 
 if (respuesta == "1") {
     alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
